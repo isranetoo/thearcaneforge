bc54a75af7cecf9003f5d660027390f21c85040a
Israel Antunes Neto <israneto20@gmail.com>
2024-09-19 17:04:12 -0300

    Version V 0.1.1

--- src/main/java/net/thearcaneforge/mod/item/ModItems.java
+++ src/main/java/net/thearcaneforge/mod/item/ModItems.java
@@ -1,7 +1,10 @@
 package net.thearcaneforge.mod.item;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -9,6 +12,8 @@ import net.minecraftforge.registries.RegistryObject;
 import net.thearcaneforge.mod.TheArcaneForge;
 import net.thearcaneforge.mod.item.custom.ArcaneChiselItem;
 import net.thearcaneforge.mod.item.custom.FueItem;
+
+import java.util.List;
 
 public class ModItems {
     public  static  final DeferredRegister<Item> ITEMS =
@@ -37,23 +42,63 @@ public class ModItems {
 
     //FOOD
     public static final RegistryObject<Item> KOHLRABI = ITEMS.register("kohlrabi",
-            () -> new Item(new Item.Properties().food(ModFoodProperties.KOHLRABI)));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.KOHLRABI)){
+
+        @Override
+                public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+                    pTooltipComponents.add(Component.translatable("tooltip.thearcaneforge.kohlrabi"));
+                    super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+                }
+            });
 
     public static final RegistryObject<Item> RADISH = ITEMS.register("radish",
-            () -> new Item(new Item.Properties().food(ModFoodProperties.RADISH)));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.RADISH)){
+                @Override
+                public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+                    pTooltipComponents.add(Component.translatable("tooltip.thearcaneforge.radish"));
+                    super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+                }
+            });
 
     public static final RegistryObject<Item> CAULIFLOWER = ITEMS.register("cauliflower",
-            () -> new Item(new Item.Properties().food(ModFoodProperties.CAULIFLOWER)));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.CAULIFLOWER)){
+                @Override
+                public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+                    pTooltipComponents.add(Component.translatable("tooltip.thearcaneforge.cauliflower"));
+                    super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+                }
+            });
 
     //FUEL
     public static final RegistryObject<Item> ARCANE_ASHES = ITEMS.register("arcane_ashes",
-            () -> new FueItem(new Item.Properties(),1200));
+            () -> new FueItem(new Item.Properties(),1200){
+
+        @Override
+                public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+                    pTooltipComponents.add(Component.translatable("tooltip.thearcaneforge.arcane_ashes"));
+                    super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+                }
+            });
 
     public static final RegistryObject<Item> GARNET_ASHES = ITEMS.register("garnet_ashes",
-            () -> new FueItem(new Item.Properties(),1200));
+            () -> new FueItem(new Item.Properties(),1200){
+
+        @Override
+                public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+                    pTooltipComponents.add(Component.translatable("tooltip.thearcaneforge.garnet_ashes"));
+                    super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+                }
+            });
 
     public static final RegistryObject<Item> FROSTFIRE_ICE = ITEMS.register("frostfire_ice",
-            () -> new FueItem(new Item.Properties(),1200));
+            () -> new FueItem(new Item.Properties(),1200){
+
+        @Override
+                public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+                    pTooltipComponents.add(Component.translatable("tooltip.thearcaneforge.frostfire_ice"));
+                    super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+                }
+            });
 
     public  static  void register (IEventBus eventBus){
 
